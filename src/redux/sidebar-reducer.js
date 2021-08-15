e7134c37259d9961209090aa01d38929c3a7e8f2
Oinari-Shinto <oinari.shinto@gmail.com>
2021-08-15 23:12:58 +0300

    small fix warning alt img

--- src/redux/sidebar-reducer.js
+++ src/redux/sidebar-reducer.js
@@ -4,9 +4,9 @@ const ADD_FRIEND = 'ADD_FRIEND';
 
 let initialState = {
     friends : [ 
-        {id:1, name: 'Tokyo', img: <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Flag_of_Tokyo_Metropolis.svg/120px-Flag_of_Tokyo_Metropolis.svg.png'></img>},
-        {id:2, name: 'Chiba', img: <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Flag_of_Chiba%2C_Chiba.svg/100px-Flag_of_Chiba%2C_Chiba.svg.png'></img>},
-        {id:3, name: 'Kyoto', img: <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Flag_of_Kyoto_City.svg/100px-Flag_of_Kyoto_City.svg.png'></img>}
+        {id:1, name: 'Tokyo', img: <img alt="Tokyo" src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Flag_of_Tokyo_Metropolis.svg/120px-Flag_of_Tokyo_Metropolis.svg.png'></img>},
+        {id:2, name: 'Chiba', img: <img alt="Chiba" src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Flag_of_Chiba%2C_Chiba.svg/100px-Flag_of_Chiba%2C_Chiba.svg.png'></img>},
+        {id:3, name: 'Kyoto', img: <img alt="Kyoto" src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Flag_of_Kyoto_City.svg/100px-Flag_of_Kyoto_City.svg.png'></img>}
     ]   
 }
 
